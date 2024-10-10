7ad72b493153b74435340861f90477db0fa2d86c
Ethan Brodie <ethan_b@optimove.com>
2024-10-10 12:45:20 +0100

    Add exit string to log.

--- consumer/src/index.js
+++ consumer/src/index.js
@@ -5,18 +5,18 @@ const { acknowledgeMessage, getMessages } = require("./queue");
 const consumerCalculator = new Calculator();
 
 async function pollQueue() {
-  const messages = await getMessages();
+    const messages = await getMessages();
 
-  for (const message of messages) {
-    console.log("Received message:", message);
+    for (const message of messages) {
+        console.log("Received message:", message);
 
-    // Parse the message body
-    // Do some calculations
-    // Output some results to console?
-  }
+        // Parse the message body
+        // Do some calculations
+        // Output some results to console?
+    }
 
-  pollQueue();
+    pollQueue();
 }
 
-console.log("Consumer started. Waiting for messages...");
+console.log("Consumer started. Waiting for messages... ctrl + c to exit");
 pollQueue();
