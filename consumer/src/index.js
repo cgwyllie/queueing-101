54c1c774f2bac38b56991cea1758acc2036ba9c6
Ethan Brodie <ethan_b@optimove.com>
2024-10-10 11:28:48 +0100

    reomve unsued import.

--- consumer/src/index.js
+++ consumer/src/index.js
@@ -1,17 +1,17 @@
-const { Calculator, Point } = require("./calculator");
+const { Calculator } = require("./calculator");
 
 const { acknowledgeMessage, getMessages } = require("./queue");
 
 const consumerCalculator = new Calculator();
 
 async function pollQueue() {
-  const messages = await getMessages();
+    const messages = await getMessages();
 
-  for (const message of messages) {
-    console.log("Received message:", message);
-  }
+    for (const message of messages) {
+        console.log("Received message:", message);
+    }
 
-  pollQueue();
+    pollQueue();
 }
 
 console.log("Consumer started. Waiting for messages...");
